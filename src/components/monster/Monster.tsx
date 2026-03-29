3d776e0429d8014facf430635e709d73cce4a0d3
simplewaholic <binhluan1806@gmail.com>
2026-03-29 07:53:40 +0700

    feat: click monster to trigger bounce animation

--- src/components/monster/Monster.tsx
+++ src/components/monster/Monster.tsx
@@ -1,18 +1,31 @@
-import { useEffect, useState } from 'react'
+// src/components/monster/Monster.tsx
+import { useEffect, useState, useCallback } from 'react'
 import './Monster.css'
 import { Constants } from '../../utils/Constants'
 
 function Monster(props: any) {
     const [sourceImg, setSourceImg] = useState('')
+    const [bouncing, setBouncing] = useState(false)
+
     useEffect(() => {
         let type = Number.parseInt(props?.monster?.Type)
         let name = props?.monster?.Name
         setSourceImg(`/assets/monsters/${Constants.TypeMonster[type]}/${name}.gif`)
     }, [props, props?.monster])
 
+    const handleClick = useCallback(() => {
+        if (bouncing) return
+        setBouncing(true)
+        setTimeout(() => setBouncing(false), 400)
+    }, [bouncing])
+
     return (
-        <div className="animation-container">
-            <img src={sourceImg} alt="monster" className="animated-gif" />
+        <div className="animation-container" onClick={handleClick}>
+            <img
+                src={sourceImg}
+                alt="monster"
+                className={`animated-gif${bouncing ? ' bounce' : ''}`}
+            />
         </div>
     )
 }
